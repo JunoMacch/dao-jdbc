0831f2fb8c67486721b50b7bb9368cdaa330d0e5
Juno <jrmacch@outlook.com>
2025-02-18 00:00:33 -0300

    criando classe de  departamento

---
+++ src/application/Principal.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Department department = new Department(1, "Techs");
+        System.out.println(department);
+
+    }
+
+}
