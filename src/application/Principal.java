a4be62dcbaaeda73b0e7a9f0b84416efbc688616
Juno <jrmacch@outlook.com>
2025-02-18 00:05:49 -0300

    Criando a classe seller

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -1,13 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Principal {
 
     public static void main(String[] args) {
 
         Department department = new Department(1, "Techs");
-        System.out.println(department);
+
+        Seller seller = new Seller(1, "Junior", "ju@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
 
     }
 
